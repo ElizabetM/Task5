df3f193c67d934d86804e2f781e227a150a38381
mareskina2000@mail.ru <mareskina2000@mail.ru>
2022-11-21 18:01:07 +0300

    Добавьте файлы проекта.

---
+++ Task5/FuncCalculateHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    public class FuncCalculateHelper
+    {
+        public double Fact(double value)
+        {
+            if ((int)value < 2)
+                return 1;
+
+            return (int)value * Fact((int)value - 1);
+        }
+
+        public double DevisionTo100(double value)
+        {
+            if (double.IsInfinity(value) || double.IsNaN(value))
+                return double.NaN;
+
+            return value / 100.0;
+        }
+
+        //логариф по основанию e
+        public double Ln(double value)
+            => Math.Log(value);
+
+        public double DoubleLn(double value)
+            => value * 2.0;
+
+        public double Exponenta(double value)
+            => Math.Exp(value);
+
+        public double Multiple(double value1, double value2)
+            => value1 * value2;
+
+        public double CalculateY(double x)
+            => Multiple(Fact(x), Exponenta(DoubleLn(Ln(DevisionTo100(x)))));
+    }
+}
---
+++ Task5Tests1/FuncCalculateHelperTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task5;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5.Tests
+{
+    [TestClass()]
+    public class FuncCalculateHelperTests
+    {
+        [TestMethod()]
+        public void FactTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void DevisionTo100Test()
+        {
+            Assert.IsTrue(true);
+
+        }
+
+        [TestMethod()]
+        public void LnTest()
+        {
+            Assert.IsTrue(true);
+
+        }
+
+        [TestMethod()]
+        public void DoubleLnTest()
+        {
+            Assert.IsTrue(true);
+
+        }
+
+        [TestMethod()]
+        public void ExponentaTest()
+        {
+            Assert.IsTrue(true);
+
+        }
+
+        [TestMethod()]
+        public void MultipleTest()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void CalculateYTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
